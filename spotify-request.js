328340839bfa8e25fec6bf73d6d120ba96681624
Joseph Cannova <jcannova@wheelership.com>
2019-03-21 13:04:42 -0400

    Displayed Artist, song, album, release data and preview url to console

--- spotify-request.js
+++ spotify-request.js
@@ -10,13 +10,17 @@ function spotifySearch(userSong){
 
     spotify
         .search({ type: 'track', query: userSong, limit:5 })
-        .then(function(response) {
+        .then( (response)=> {
                 console.log("Search for: ", userSong);
+                console.log("\n");
+
             for(let i =0;  i < response.tracks.items.length; i++) {
 
                 console.log("Artist: ", response.tracks.items[i].album.artists[0].name);
+                console.log("Song: ",response.tracks.items[i].name);
                 console.log("Album: ", response.tracks.items[i].album.name);
                 console.log("Release Date: ", response.tracks.items[i].album.release_date);
+                console.log("Preview: ",response.tracks.items[i].preview_url);
                 console.log("\n------------\n");
 
             }
@@ -25,6 +29,8 @@ function spotifySearch(userSong){
         .catch(function(err) {
             console.log(err);
         });
+
+    console.log("Im At the end of the function");
 }
 module.exports = spotifySearch;
 
